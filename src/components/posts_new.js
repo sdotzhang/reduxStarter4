a9ded7d90127b54afbabf7fea7c53a1e8b542e32
Shaonan Zhang <sdotzhang@gmail.com>
2017-10-16 09:25:02 -0700

    added call back after post created
    Change-Id: I0f65123ced935a88001ca1b93fb5f0b0a8882b53

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -25,8 +25,9 @@ class PostsNew extends Component {
     }
 
     onSubmit(values) {
-        console.log(values)
-        this.props.createPost(values)
+        this.props.createPost(values, () => {
+            this.props.history.push('/')
+        })
     }
 
     render() {
