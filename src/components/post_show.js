b676654141c96da06c45cb2b985e5fbf476d36ad
Shaonan Zhang <sdotzhang@gmail.com>
2017-10-23 08:53:50 -0700

    render post with fetched data
    Change-Id: I7db806d1c4c321f3f72aa6f4c8bf0817482ff6ca

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
 
 class PostShow extends Component {
+
+    componentDidMount() {
+        const { id } = this.props.match.params
+        this.props.fetchPost(id)
+    }
+
     render() {
+        const { post } = this.props;
+        if (!post) {
+            return (
+                <div>Loading ... </div>
+            )
+        }
         return (
             <div>
-                POST SHOW !
+                <h3>{post.title}</h3>
+                <h6>Category: {post.category}</h6>
+                <p>{post.content}</p>
             </div>
         )
     }
 }
 
+function mapStateToProps ({ posts }, ownProps) {
+    return {
+        post: posts[ownProps.match.params.id]
+    }
+}
 
-export default PostShow;
+
+export default connect(mapStateToProps, { fetchPost })(PostShow);
